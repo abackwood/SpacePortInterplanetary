27eac1b5705594419db988611f47aa4942fdba4c
abackwood <a.backwood@gmail.com>
2016-09-24 16:04:25 +0200

    Added XML reading functions for Ship and Engine

--- Assets/Models/Ship/Engine.cs
+++ Assets/Models/Ship/Engine.cs
@@ -1,18 +1,81 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace Ship
 {
-    public class Engine
+    public class Engine : IXmlSerializable
     {
         public string ID { get; private set; }
 
         public Vector3 Position { get; private set; }
 
-        public Vector3 Direction { get; private set; }
+        public Vector3 Orientation { get; private set; }
 
         public float MaxThrust { get; private set; }
 
+        public float Diameter { get; private set; }
+
         public float Power { get; set; }
+
+        public Engine(string id)
+        {
+            this.ID = id;
+        }
+
+        public Engine(string id, Engine proto)
+        {
+            this.ID = id;
+            this.MaxThrust = proto.MaxThrust;
+            this.Diameter = proto.Diameter;
+        }
+
+        #region IXmlSerializable implementation
+
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader parentReader)
+        {
+            XmlReader reader = parentReader.ReadSubtree();
+            while (reader.Read())
+            {
+                switch (reader.Name)
+                {
+                    case "Position":
+                        float x = float.Parse(reader.GetAttribute("x"));
+                        float y = float.Parse(reader.GetAttribute("y"));
+                        float z = float.Parse(reader.GetAttribute("z"));
+                        Position = new Vector3(x,y,z);
+                        break;
+                    case "Orientation":
+                        x = float.Parse(reader.GetAttribute("x"));
+                        y = float.Parse(reader.GetAttribute("y"));
+                        z = float.Parse(reader.GetAttribute("z"));
+                        Orientation = new Vector3(x,y,z);
+                        break;
+                    case "Thrust":
+                        reader.Read();
+                        MaxThrust = reader.ReadContentAsFloat();
+                        break;
+                    case "Diameter":
+                        reader.Read();
+                        Diameter = reader.ReadContentAsFloat();
+                        break;
+                }
+            }
+
+            reader.Close();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            
+        }
+
+        #endregion
     }
 }
--- Assets/Models/Ship/Ship.cs
+++ Assets/Models/Ship/Ship.cs
@@ -1,18 +1,95 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace Ship
 {
-    public class Ship
+    public class Ship : IXmlSerializable
     {
         private Dictionary<string,Engine> engines;
 
         public string ID { get; private set; }
 
+        public string Name { get; private set; }
+
         public Vector3d Position { get; set; }
 
-        public float Mass { get; set; }
+        public float DryMass { get; set; }
 
         public Vector3 CenterOfMass { get; set; }
+
+        public Ship(string id)
+        {
+            this.ID = id;
+            engines = new Dictionary<string,Engine>();
+        }
+
+        #region IXmlSerializable implementation
+
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            
+        }
+
+        #endregion
+
+        public void ReadPrototypeXml(XmlReader parentReader)
+        {
+            XmlReader reader = parentReader.ReadSubtree();
+            while (reader.Read())
+            {
+                switch (reader.Name)
+                {
+                    case "Name":
+                        reader.Read();
+                        Name = reader.ReadContentAsString();
+                        break;
+                    case "Mass":
+                        reader.Read();
+                        DryMass = reader.ReadContentAsFloat();
+                        break;
+                    case "CenterOfMass":
+                        float x = float.Parse(reader.GetAttribute("x"));
+                        float y = float.Parse(reader.GetAttribute("y"));
+                        float z = float.Parse(reader.GetAttribute("z"));
+                        CenterOfMass = new Vector3(x,y,z);
+                        break;
+                    case "Engines":
+                        ReadEnginesXml(reader);
+                        break;
+                }
+            }
+
+            reader.Close();
+        }
+
+        private void ReadEnginesXml(XmlReader parentReader)
+        {
+            XmlReader reader = parentReader.ReadSubtree();
+            if(reader.ReadToDescendant("Engine"))
+            {
+                do
+                {
+                    string engineID = reader.GetAttribute("id");
+                    Engine engine = new Engine(engineID);
+                    engine.ReadXml(reader);
+                    engines.Add(engineID,engine);
+                }
+                while(reader.ReadToNextSibling("Engine"));
+            }
+
+            reader.Close();
+        }
     }
 }
