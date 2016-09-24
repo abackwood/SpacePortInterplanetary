abadfee4c1fce27d1f4669968a3e07592199f9a0
abackwood <a.backwood@gmail.com>
2016-09-24 13:13:18 +0200

    Added Ship and Engine classes

---
+++ Assets/Models/Ship/Engine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    public class Engine
+    {
+        public string ID { get; private set; }
+
+        public Vector3 Position { get; private set; }
+
+        public Vector3 Direction { get; private set; }
+
+        public float MaxThrust { get; private set; }
+
+        public float Power { get; set; }
+    }
+}
---
+++ Assets/Models/Ship/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    public class Ship
+    {
+        private Dictionary<string,Engine> engines;
+
+        public string ID { get; private set; }
+
+        public Vector3d Position { get; set; }
+
+        public float Mass { get; set; }
+
+        public Vector3 CenterOfMass { get; set; }
+    }
+}
